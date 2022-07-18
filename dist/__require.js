38a21c93c8dfe8428d8195e4ce9e7740912eb6a9
68180663+undefined@users.noreply.github.com <68180663+undefined@users.noreply.github.com>
2022-07-18 19:48:43 +0000

    deploy Mon Jul 18 2022 19:48:43 GMT+0000 (Coordinated Universal Time)

---
+++ dist/__require.js
@@ -0,0 +1,12 @@
+
+const modules = ['@vladmandic/human'];
+export default function require(library) {
+  const idx = modules.findIndex(
+    (it) =>
+      it === library ||
+      it.replace(/^((@[^/]*\/)?[^/@]*)(@[^/]*)?(\/[^@]*)?$/, '$1$4') ===
+        library // removes version pinned, if any
+  );
+  if (idx === -1) throw new Error(`Import ${library} not found in project scope: ${modules}`);
+  return import(`https://srv.divriots.com/packd/@vladmandic/human`).then((module) => module['packd_export_'+idx]);
+}
---
+++ dist/index.js
@@ -0,0 +1,60 @@
+import { packd_export_0 } from 'https://srv.divriots.com/packd/@vladmandic/human';function unwrapExports(x){return x&&x.__esModule&&Object.prototype.hasOwnProperty.call(x,"default")?x["default"]:x}const Human = unwrapExports(packd_export_0.default);;
+
+class MyCounter extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = `<header> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/human/2.1.5/human.js"></script>
+    </header>
+  <body>  
+    <video
+      id="inputVideo"
+      autoplay
+      muted
+      width="640"
+      height="480"
+      style="border: 1px solid #ddd;"
+    ></video
+    ><br/>
+    <canvas
+      id="overlay"
+      width="640"
+      height="480"
+      style="position: relative; top: -487px; border: 1px solid #ddd;"
+    ></canvas
+    ><br/>
+  </body>
+    `;
+  }
+
+  connectedCallback() {
+    console.log('start');
+    run();
+
+    async function run() {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: {}
+      });
+      const videoEl = document.getElementById("inputVideo");
+      videoEl.srcObject = stream;
+      const config = {
+        backend: 'webgl'
+      };
+      const human = new Human(config); // create instance of Human
+
+      const inputVideo = document.getElementById("inputVideo");
+      const outputCanvas = document.getElementById('overlay');
+      /*
+                  async function detectVideo() {
+                      const result = await human.detect(inputVideo); // run detection
+                      human.draw.all(outputCanvas, result); // draw all results
+                      requestAnimationFrame(detectVideo); // run loop
+                  }
+      */
+      //detectVideo()
+    }
+  }
+
+}
+
+customElements.define("my-counter", MyCounter);
